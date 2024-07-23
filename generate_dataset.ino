c1a2065d89ba3bfa2630372dfd7d00738312f46d
arman <arthomas2878@gmail.com>
2024-07-23 09:40:14 +0200

    STEP 1 : Audio setup

---
+++ generate_dataset.ino
@@ -0,0 +1,53 @@
+#include <WiFiNINA.h>
+#include <PDM.h>
+
+// default number of output channels
+static const char channels = 1;
+
+// default PCM output frequency
+static const int frequency = 16000;
+
+// Buffer to read samples into, each sample is 16-bits
+short sampleBuffer[512];
+
+// Number of audio samples read
+volatile int samplesRead;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  PDM.onReceive(onPDMdata);
+
+  if (!PDM.begin(channels, frequency)) {
+    Serial.println("Failed to start PDM!");
+    while (1);
+  }
+}
+
+void loop() {
+  if (samplesRead) {
+    for (int i = 0; i < samplesRead; i++) {
+      if (channels == 2) {
+        Serial.print("L:");
+        Serial.print(sampleBuffer[i]);
+        Serial.print(" R:");
+        i++;
+      }
+      Serial.println(sampleBuffer[i]);
+    }
+    
+    // Ajoute un délai pour une meilleure visualisation
+    delay(100); // Ajustez la valeur en millisecondes selon vos besoins
+  }
+}
+
+void onPDMdata() {
+  // Query the number of available bytes
+  int bytesAvailable = PDM.available();
+
+  // Read into the sample buffer
+  PDM.read(sampleBuffer, bytesAvailable);
+
+  // 16-bit, 2 bytes per sample
+  samplesRead = bytesAvailable / 2;
+}
